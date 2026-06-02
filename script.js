1a87e2f0a7d9557ae8045445f704f171392b5a8e
nassu <1776306422@qq.com>
2026-06-02 09:45:10 +0800

    1

--- script.js
+++ script.js
@@ -16,8 +16,7 @@ async function loadArticle(path) {
   // 数学公式渲染
   renderMathInElement(article, {
     delimiters: [
-      { left: "$$", right: "$$", display: true },
-      { left: "$", right: "$", display: false }
+      { left: "$$", right: "$$", display: true }
     ]
   })
 
